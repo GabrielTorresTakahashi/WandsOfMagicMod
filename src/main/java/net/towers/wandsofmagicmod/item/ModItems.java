8ecb202339457795a4f2e6a788fdbfbb3e3bc9b2
Gabriel Torres Takahashi <gabrielspot1234@gmail.com>
2022-12-20 16:05:48 -0300

    Wand of Fire in progress...

---
+++ src/main/java/net/towers/wandsofmagicmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.towers.wandsofmagicmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.towers.wandsofmagicmod.WandsOfMagicMod;
+
+public class ModItems {
+
+    private static Item registerItem(String name, Item item, ItemGroup group) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+        return Registry.register(Registries.ITEM, new Identifier(WandsOfMagicMod.MOD_ID, name), item);
+    }
+
+    public static final Item FIRE_WAND = registerItem(
+            "fire_wand",
+            new Item(new Item.Settings()),
+            ItemGroups.COMBAT);
+
+    public static void registerModItems() {
+        WandsOfMagicMod.LOGGER.debug("Registering Mod Items for " + WandsOfMagicMod.MOD_ID);
+    }
+
+}
