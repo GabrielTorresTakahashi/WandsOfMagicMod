8717fa803bd334d33f9d65a4520e76913745a69a
Gabriel Torres Takahashi <gabrielspot1234@gmail.com>
2022-12-20 15:46:40 -0300

    first commit

---
+++ src/main/java/net/towers/wandsofmagicmod/WandsOfMagicModClient.java
@@ -0,0 +1,11 @@
+package net.towers.wandsofmagicmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WandsOfMagicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
