c9eacad519bad2ced1791238867ac1b88a144fec
Gabriel Torres Takahashi <gabrielspot1234@gmail.com>
2022-12-22 11:08:09 -0300

    Nerfed explosion power, Wand of Fire texture done

--- src/main/java/net/towers/wandsofmagicmod/entity/projectile/thrown/FireProjectileEntity.java
+++ src/main/java/net/towers/wandsofmagicmod/entity/projectile/thrown/FireProjectileEntity.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.passive.SnowGolemEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.util.hit.BlockHitResult;
@@ -43,14 +42,14 @@ public class FireProjectileEntity extends ThrownItemEntity {
     protected void onEntityHit(EntityHitResult entityHitResult) {
         super.onEntityHit(entityHitResult);
         Entity entity = entityHitResult.getEntity();
-        int damage = entity instanceof SnowGolemEntity ? 10 : 5;
+        int damage = explosion ? 20 : 5;
         entity.setFireTicks(40);
         entity.damage(DamageSource.thrownProjectile(this, this.getOwner()), damage);
     }
 
     @Override
     protected void onCollision(HitResult hitResult) {
-        int explosionPower = 2;
+        int explosionPower = 1;
         super.onCollision(hitResult);
         if (!this.world.isClient) {
             this.world.sendEntityStatus(this, EntityStatuses.PLAY_DEATH_SOUND_OR_ADD_PROJECTILE_HIT_PARTICLES);
